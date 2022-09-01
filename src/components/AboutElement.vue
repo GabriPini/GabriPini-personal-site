4a4192624bf4833e2575ec3b52fca9f197067b57
GabriPini <lele19977@gmail.com>
2022-09-01 16:21:42 +0200

    fix

--- src/components/AboutElement.vue
+++ src/components/AboutElement.vue
@@ -5,7 +5,7 @@
       <p class="lead text-white">
         <em>
           My name is Gabriele Pini and I have recently completed an online
-          accademy with BooleanCareers. I have a passion for all things
+          academy with BooleanCareers. I have a passion for all things
           technology and design, from software engineering to UI/UX. In addition
           to my love of technology and design, I am also interested in
           videogames. Below are details of some of the projects I have developed
